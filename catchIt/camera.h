d8f456470b04d9dabada762879b0e5735a3dfbd0
Drew <drewsumsion10@gmail.com>
2020-03-02 15:19:07 -0700

    Here are some updates. Gotta learn GitHub sometime

--- catchIt/camera.h
+++ catchIt/camera.h
@@ -5,26 +5,38 @@
 #ifndef CATCHIT_CAMERA_H
 #define CATCHIT_CAMERA_H
 
+#define VIDEO_RESOLUTION_X 640
+#define VIDEO_RESOLUTION_Y 480
+
 class cameraControl {
 public:
-    cameraControl();
+    cameraControl();        //sets up everything when defined.
     bool isDone();      //returns if the camera portion is done or not
-
     double getInitialXVelocity();
     double getInitialYVelocity();
     double getInitialZVelocity();
 
 private:
     bool trackingStatus;        //false means it is done tracking
+    Vec2i videoResolution;
+    VideoCapture theSourceVideo;        //change it in the cameraControl() function if want something besides the camera.
 };
 
 cameraControl cameraControl::cameraControl() {
-    trackingStatus = false;
+    trackingStatus = false;     //then when it checks it knows it isn't done.
+
+    videoResolution = {VIDEO_RESOLUTION_X, VIDEO_RESOLUTION_Y};       //any more intense will make the pie too slow.
+
+    theSourceVideo = VideoCapture(0, CAP_V4L2);
+    theSourceVideo.set(CAP_PROP_FRAME_WIDTH, VIDEO_RESOLUTION_X);
+    theSourceVideo.set(CAP_PROP_FRAME_WIDTH, VIDEO_RESOLUTION_Y);
+
+    if(!theSourceVideo.isOpened()) {
+        std::cout << "Ok. Something went wrong. The camera didn't open right." << std::endl;
+    }
 }
 
 bool cameraControl::isDone() {
-    std::cout << "test" << std::endl;
-
     return trackingStatus;
 }
 
