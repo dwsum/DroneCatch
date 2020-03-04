a615bdba5d1b98bf52b8f1c945f38e929bc24d95
Drew <drewsumsion10@gmail.com>
2020-03-04 15:33:39 -0700

    taking it back to basics

--- catchIt/cmake-build-debug/CMakeFiles/catchIt.dir/DependInfo.cmake
+++ catchIt/cmake-build-debug/CMakeFiles/catchIt.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/li292/CLionProjects/revisedDroneCatch/catchIt/main.cpp" "/home/li292/CLionProjects/revisedDroneCatch/catchIt/cmake-build-debug/CMakeFiles/catchIt.dir/main.cpp.o"
+  "/home/drew/Documents/Drew/Drone/DroneCatch/catchIt/main.cpp" "/home/drew/Documents/Drew/Drone/DroneCatch/catchIt/cmake-build-debug/CMakeFiles/catchIt.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
